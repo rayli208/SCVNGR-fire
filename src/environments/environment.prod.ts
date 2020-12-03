f60d9acb14a047e7a61a4bd035218a37623659fa
Rayli Raykov <rayli208@gmail.com>
2020-12-03 11:24:42 -0500

    first commmit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true ,
+  firebase: {
+    apiKey: "AIzaSyBm4URzMF2oCpwV1SQJla-yvwIEkr61_l8",
+    authDomain: "scvngr-firedb-f0813.firebaseapp.com",
+    databaseURL: "https://scvngr-firedb.firebaseio.com",
+    projectId: "scvngr-firedb-f0813",
+    storageBucket: "scvngr-firedb-f0813.appspot.com",
+    messagingSenderId: "149441352158",
+    appId: "1:149441352158:web:a6f59a72a00329d1d70d4f",
+    measurementId: "G-55YJYEQ65S"  
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBm4URzMF2oCpwV1SQJla-yvwIEkr61_l8",
+    authDomain: "scvngr-firedb-f0813.firebaseapp.com",
+    databaseURL: "https://scvngr-firedb.firebaseio.com",
+    projectId: "scvngr-firedb-f0813",
+    storageBucket: "scvngr-firedb-f0813.appspot.com",
+    messagingSenderId: "149441352158",
+    appId: "1:149441352158:web:a6f59a72a00329d1d70d4f",
+    measurementId: "G-55YJYEQ65S"
+  }
 };
 
 /*
